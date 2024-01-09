e279a7638dac5e9c84525e263fea404691e31879
Zuan0x <zuan.0x@gmail.com>
2024-01-09 12:49:18 +0000

    Add catch feature

--- commands.go
+++ commands.go
@@ -5,9 +5,8 @@ import (
 	"os"
 	"os/exec"
 	"errors"
+	"math/rand"
 )
-
-
 
  func commandHelp(cfg *config, args ...string) error {
 	fmt.Println()
@@ -84,5 +83,30 @@ func commandExplore(cfg *config, args ...string) error {
 	for _, enc := range location.PokemonEncounters {
 		fmt.Printf(" - %s\n", enc.Pokemon.Name)
 	}
+	return nil
+}
+
+func commandCatch(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("you must provide a pokemon name")
+	}
+
+	name := args[0]
+	pokemon, err := cfg.pokeapiClient.GetPokemon(name)
+	if err != nil {
+		return err
+	}
+
+	res := rand.Intn(pokemon.BaseExperience)
+
+	fmt.Printf("Throwing a Pokeball at %s...\n", pokemon.Name)
+	if res > 40 {
+		fmt.Printf("%s escaped!\n", pokemon.Name)
+		return nil
+	}
+
+	fmt.Printf("%s was caught!\n", pokemon.Name)
+
+	cfg.caughtPokemon[pokemon.Name] = pokemon
 	return nil
 }
